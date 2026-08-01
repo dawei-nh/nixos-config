bb96d21834373c3482836cbe0e704303fff7d646
David Huynh <davidnhuynh@gmail.com>
2026-08-01 09:10:40 -0500

    Fix PIA desktop shortcut

--- pkgs/piavpn.nix
+++ pkgs/piavpn.nix
@@ -70,7 +70,7 @@ stdenv.mkDerivation rec {
       name = pname;
       desktopName = "Private Internet Access (PIA)";
       comment = "Private Internet Access VPN client";
-      exec = "${placeholder "out"}/bin/pia-client %u";
+      exec = "/opt/piavpn/bin/pia-client-wrapped %u";
       icon = pname;
       terminal = false;
       categories = [ "Network" ];
